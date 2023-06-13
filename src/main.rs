8e3f4bfda03e44cd97e10fe75615e46462f27808
gajevski <mikolajxgajewski@gmail.com>
2023-06-13 15:34:43 +0200

    Test CI pipe

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
+    let name = req.match_info().get("name").unwrap_or("Testing CI!");
     format!("Hello {}!", &name)
 }
 #[tokio::main]
